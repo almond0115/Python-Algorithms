718d948e1461550eea4b5823abca06541e90927b
almond0115 <jjh19960115@gmail.com>
2022-12-05 23:13:06 +0900

    gold2_5214

--- BaekJoon/gold2_5214.py
+++ BaekJoon/gold2_5214.py
@@ -1,6 +1,8 @@
 # 문제
 # 아주 먼 미래에 사람들이 가장 많이 사용하는 대중교통은 하이퍼튜브이다.
 # 하이퍼튜브 하나는 역 K개를 서로 연결한다. 1번역에서 N번역으로 가는데 방문하는 최소 역의 수는 몇 개일까?
+import sys
+from collections import deque
 
 # 입력
 # 첫째 줄에 역의 수 N과 한 하이퍼튜브가 서로 연결하는 역의 개수 K, 하이퍼튜브의 개수 M이 주어진다. (1 ≤ N ≤ 100,000, 1 ≤ K, M ≤ 1000)
@@ -9,4 +11,42 @@
 # 출력
 # 첫째 줄에 1번역에서 N번역으로 가는데 방문하는 역의 개수의 최솟값을 출력한다. 만약, 갈 수 없다면 -1을 출력한다.
 
-#
+# 예제 입력
+# 9 3 5
+# 1 2 3
+# 1 4 5
+# 3 6 7
+# 5 6 7
+# 6 8 9
+
+# 예제 출력
+# 4
+
+n, k, m = map(int, input().split())
+subway = [[] for _ in range(n+m+1)]
+
+for idx in range(1, m+1):
+    tube_nums = list(map(int, input().split()))
+    subway[n+idx] = tube_nums
+    for s in tube_nums:
+        subway[s].append(n+idx)
+
+q = deque()
+isVisited = [False] * (n+m+1)
+q.append((1, isVisited, 1))
+isVisited[1] = True
+
+while q:
+    station, visited, stage = q.popleft()
+    if station == n:
+        print('1번역에서 N번역으로 가는데 방문하는 역의 개수의 최솟값 = %d' % stage)
+        sys.exit()
+
+    for i in subway[station]:
+        if visited[i] == False:
+            visited[i] = True
+            if i > n:
+                q.append((i, visited, stage))
+            else:
+                q.append((i, visited, stage+1))
+print(-1)
